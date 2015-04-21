a759013ff7b8a99de4ccd65bfaf74ffc0b178f6b
Gentry Rolofson <xdarkicex@gmail.com>
2015-04-21 01:29:23 -0700

    DEMO

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,5 +1,5 @@
 function toggle(x){
-    var panel = document.getElementById(x), maxH="500px";
+    var panel = document.getElementById(x), maxH="400px";
     if(panel.style.height == maxH){
         panel.style.height = "0px";
         
