06ed2312936c83688a96e0545706d0703228ebf6
Gentry Rolofson <xdarkicex@gmail.com>
2015-04-22 01:40:39 -0700

    ..

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -9,9 +9,9 @@ function toggle(x){
     }
 }
 
-function close(slidemenu){
-	var menu = document.getElementById('slidemenu'), maxH="400px";
-	if (menu.style.height == maxH){
+function close(x){
+	var menu = document.getElementById(x), maxH="400px";
+	if (menu.style.height === maxH){
 		menu.style.height = "0px";
 	}
 }
